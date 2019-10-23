f3c300fa18d55276441d743fde7994a477b95554
MaxStepanov90 <MAXSTEPANOV90@mail.ru>
2019-10-23 17:52:04 +0300

    Commit 1

---
+++ src/main/java/com/vebinar/dao/UserDaoImpl.java
@@ -0,0 +1,47 @@
+package com.vebinar.dao;
+
+import com.vebinar.entity.User;
+import com.vebinar.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public class UserDaoImpl implements UserDao {
+
+
+    @Autowired
+    public JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void save(User user) {
+        String sql = "INSERT INTO user (name, email, age) VALUES (?,?,?)";
+        jdbcTemplate.update(sql,user.getName(),user.getEmail(),user.getAge());
+    }
+
+    @Override
+    public User getById(int id) {
+        String sql = "SELECT * FROM user WHERE id=?";
+        return jdbcTemplate.queryForObject(sql, new UserMapper(), id);
+    }
+
+    @Override
+    public void update(User user) {
+        String sql = "UPDATE user SET name=?, email=?, age=? WHERE id=?";
+        jdbcTemplate.update(sql,user.getName(),user.getEmail(),user.getAge(),user.getId());
+    }
+
+    @Override
+    public void delete(int id) {
+        String sql = "DELETE FROM user WHERE id=?";
+        jdbcTemplate.update(sql,id);
+
+    }
+
+    @Override
+    public List<User> findAll() {
+        String sql = "SELECT*FROM user";
+        return jdbcTemplate.query(sql, new UserMapper());
+    }
+}
